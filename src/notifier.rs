fa2e9da82886bebb25558993d37fe184019b217e
ybbh <guohua16@outlook.com>
2023-12-11 16:13:10 +0800

    scupt-net

--- src/notifier.rs
+++ src/notifier.rs
@@ -1,12 +1,8 @@
 use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
-use std::sync::atomic::Ordering::SeqCst;
-use std::thread;
-use std::time::Duration;
+use std::sync::atomic::{AtomicBool,  Ordering};
 
 use tokio::sync::Notify;
-use tokio::task;
-use tokio::time::sleep;
+
 use tracing::trace;
 
 // Notifies tasks to wake up.
@@ -21,8 +17,7 @@ pub struct Notifier {
 
 pub struct NotifyInner {
     stop_notifier: Notify,
-    stopped: AtomicBool,
-    num_waiting: AtomicU64,
+    stopped: AtomicBool
 }
 
 impl Default for Notifier {
@@ -59,21 +54,12 @@ impl Notifier {
     pub fn task_notify_all(&self) -> bool {
         trace!("notify waiter {}", self.name);
         let ret = self.inner.notify_all();
-        let inner = self.inner.clone();
-
-        let _r = task::spawn(async move {
-            inner.task_repeat_notify_until_no_waiting().await;
-        });
         ret
     }
 
     pub fn notify_all(&self) -> bool {
         trace!("notify waiter {}", self.name);
         let ret = self.inner.notify_all();
-        let inner = self.inner.clone();
-        let _s = thread::Builder::new().spawn(move || {
-            inner.repeat_notify_until_no_waiting();
-        });
         ret
     }
 }
@@ -82,17 +68,14 @@ impl NotifyInner {
     fn new() -> Self {
         Self {
             stopped: AtomicBool::new(false),
-            stop_notifier: Notify::new(),
-            num_waiting: AtomicU64::new(0),
+            stop_notifier: Notify::new()
         }
     }
 
     async fn notified(&self) {
-        self.num_waiting.fetch_add(1, Ordering::SeqCst);
         if !self.stopped.load(Ordering::SeqCst) {
             self.stop_notifier.notified().await;
         }
-        self.num_waiting.fetch_sub(1, Ordering::SeqCst);
     }
 
     fn is_notified(&self) -> bool {
@@ -116,22 +99,6 @@ impl NotifyInner {
             }
         };
 
-
         ret
-    }
-
-    // Keep invoking notify_waiters until the num_waiting is 0.
-    async fn task_repeat_notify_until_no_waiting(&self) {
-        while self.num_waiting.load(SeqCst) != 0 {
-            sleep(Duration::from_millis(10)).await;
-            self.stop_notifier.notify_waiters();
-        }
-    }
-
-    fn repeat_notify_until_no_waiting(&self) {
-        while self.num_waiting.load(SeqCst) != 0 {
-            thread::sleep(Duration::from_millis(10));
-            self.stop_notifier.notify_waiters();
-        }
     }
 }
